1593ae9a456bbc9aed11874ada8678222a6c0ad1
ethonwilles <ethonwilles@gmail.com>
2020-06-15 15:34:26 -0600

    adsf

---
+++ static/precache-manifest.da95fd0f66411cd9f4aaa98d604c8d9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569734267307febc96fd",
+    "url": "/static/ css/main.59f35602.chunk.css"
+  },
+  {
+    "revision": "23442a36681249205196",
+    "url": "/static/ js/2.5a1dde64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/ js/2.5a1dde64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "569734267307febc96fd",
+    "url": "/static/ js/main.060f54e5.chunk.js"
+  },
+  {
+    "revision": "190b797c4840bbfef3e7",
+    "url": "/static/ js/runtime-main.cd4add69.js"
+  },
+  {
+    "revision": "e32130966bcd325a827dbf5b5c5f58fc",
+    "url": "/static/../templates/index.html"
+  }
+]);
